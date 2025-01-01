fe9223cb4b81294e1ffbb950c9d0b6d715a81877
Weichen Xu <weichen.xu@databricks.com>
2025-01-02 00:34:16 +0800

    update
    Signed-off-by: Weichen Xu <weichen.xu@databricks.com>

--- torch/_functorch/autograd_function.py
+++ torch/_functorch/autograd_function.py
@@ -394,7 +394,7 @@ def vmapify_autograd_function(autograd_function, in_dims, batch_size, randomness
         outputs, out_dims = restore_vmap(
             autograd_function.forward, in_dims, batch_size, randomness
         )(*operands)
-        return *outputs, out_dims
+        return outputs, out_dims
 
     def setup_context(ctx, inputs, outputs):
         origin_outputs = outputs[:-1]
